4ba17c5908e80e272081aba9e29bfbd56770c7f7
Meitar Basson <meitarbass@gmail.com>
2020-04-15 01:34:03 +0300

    Added UI components

---
+++ todoApp/Controller/CategoryVC.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryVC.swift
+//  todoApp
+//
+//  Created by Meitar Basson on 15/04/2020.
+//  Copyright © 2020 Meitar Basson. All rights reserved.
+//
+
+import UIKit
+
+class CategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
